245aad8ca229913aeecbd2f106196a7ffa04680b
TwentyfiveBTea <1248102041@qq.com>
2025-10-13 15:01:33 +0800

    引入解决 SpringBoot 项目请求接口无效字符问题代码类

---
+++ src/main/java/com/btea/wxgoutmanage/common/config/TomcatConfig.java
@@ -0,0 +1,25 @@
+package com.btea.wxgoutmanage.common.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/10/13 09:24
+ * @Description: 用于解决 SpringBoot 项目请求接口无效字符问题
+ */
+@Configuration
+public class TomcatConfig {
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((Connector connector) -> {
+            connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+            connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+        });
+        return factory;
+    }
+}
+
