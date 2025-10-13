29d510255d704f844b5e182a140a9b115fa02ba7
TwentyfiveBTea <1248102041@qq.com>
2025-10-13 15:02:19 +0800

    初始化 User 实体类

---
+++ src/main/java/com/btea/wxgoutmanage/dao/entity/UserDO.java
@@ -0,0 +1,60 @@
+package com.btea.wxgoutmanage.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/10/13 14:07
+ * @Description: 用户实体类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("t_user")
+public class UserDO {
+    /**
+     * 主键id
+     */
+    private Integer id;
+
+    /**
+     * 用户小程序唯一标识
+     */
+    private String openid;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+}
