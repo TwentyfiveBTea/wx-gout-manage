1271a7420730ee164ec26f6e0c9a9ad18a27f3d8
TwentyfiveBTea <1248102041@qq.com>
2025-10-13 12:22:51 +0800

    项目初始化

---
+++ src/main/java/com/btea/wxgoutmanage/WxGoutManagerApplication.java
@@ -0,0 +1,13 @@
+package com.btea.wxgoutmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxGoutManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxGoutManagerApplication.class, args);
+    }
+
+}
