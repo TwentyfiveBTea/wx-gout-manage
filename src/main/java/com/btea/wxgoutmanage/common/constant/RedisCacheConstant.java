a1ff5e22579a1de2daec61eb507039efd99b8b8b
TwentyfiveBTea <1248102041@qq.com>
2025-10-14 10:58:58 +0800

    新增：短信验证码缓存标识

---
+++ src/main/java/com/btea/wxgoutmanage/common/constant/RedisCacheConstant.java
@@ -0,0 +1,15 @@
+package com.btea.wxgoutmanage.common.constant;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/10/13 18:57
+ * @Description: Reids 缓存常量类
+ */
+public class RedisCacheConstant {
+
+    /**
+     * 短信验证码缓存标识
+     */
+    public static final String SMS_CODE_CACHE_KEY = "sms_code:";
+
+}
