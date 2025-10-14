3c256253b4fbde5a9dbd73c44a333ad607d29284
TwentyfiveBTea <1248102041@qq.com>
2025-10-14 10:26:34 +0800

    新增：自定义客户端异常

---
+++ src/main/java/com/btea/wxgoutmanage/common/convention/exception/ClientException.java
@@ -0,0 +1,20 @@
+package com.btea.wxgoutmanage.common.convention.exception;
+
+import com.btea.wxgoutmanage.common.convention.errorcode.BaseErrorCode;
+import com.btea.wxgoutmanage.common.convention.errorcode.IErrorCode;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/10/14 10:19
+ * @Description: 客户端异常
+ */
+public class ClientException extends AbstractException{
+
+    public ClientException(String message) {
+        super(message, null, BaseErrorCode.CLIENT_ERROR);
+    }
+
+    public ClientException(String message, Throwable throwable) {
+        super(message, throwable, BaseErrorCode.CLIENT_ERROR);
+    }
+}
