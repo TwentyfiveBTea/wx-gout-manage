67aeb177fa4f314a47cb00f9a25fb280da39a834
TwentyfiveBTea <1248102041@qq.com>
2025-10-14 19:54:30 +0800

    新增：新增用户上下文配置类

---
+++ src/main/java/com/btea/wxgoutmanage/common/context/UserContext.java
@@ -0,0 +1,36 @@
+package com.btea.wxgoutmanage.common.context;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/10/14 19:48
+ * @Description: 用户上下文
+ */
+public class UserContext {
+
+    private static final ThreadLocal<String> USER_THREAD_LOCAL = new ThreadLocal<>();
+
+    /**
+     * 设置当前用户ID
+     *
+     * @param userId 用户ID
+     */
+    public static void setUserId(String userId) {
+        USER_THREAD_LOCAL.set(userId);
+    }
+
+    /**
+     * 获取当前用户ID
+     *
+     * @return 用户ID
+     */
+    public static String getUserId() {
+        return USER_THREAD_LOCAL.get();
+    }
+
+    /**
+     * 清除当前用户ID
+     */
+    public static void clear() {
+        USER_THREAD_LOCAL.remove();
+    }
+}
