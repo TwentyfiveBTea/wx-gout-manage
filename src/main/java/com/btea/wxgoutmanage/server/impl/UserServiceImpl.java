c46a08bc9335a5f5b21b06deae96358135681bf3
TwentyfiveBTea <1248102041@qq.com>
2025-10-15 13:32:00 +0800

    新增：实现用户注册功能

--- src/main/java/com/btea/wxgoutmanage/server/impl/UserServiceImpl.java
+++ src/main/java/com/btea/wxgoutmanage/server/impl/UserServiceImpl.java
@@ -34,19 +34,17 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
      */
     @Override
     public void register(UserRegisterRespDTO requestParam) {
-        log.info("开始用户注册，用户名: {}, 密码1: {}, 密码2:{}, 手机号: {}, 验证码: {}", requestParam.getUsername(), requestParam.getPassword1(), requestParam.getPassword2(), requestParam.getPhone(), requestParam.getCode());
-        if (userMapper.selectOne(null) != null) {
+        log.info("开始用户注册，用户名: {}, 密码1: {}, 密码2:{}, 手机号: {}", requestParam.getUsername(), requestParam.getPassword1(), requestParam.getPassword2(), requestParam.getPhone());
+        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
+                .eq(UserDO::getUsername, requestParam.getUsername());
+        if (userMapper.selectOne(queryWrapper) != null) {
             throw new ClientException("用户已存在");
         }
         if (!Objects.equals(requestParam.getPassword1(), requestParam.getPassword2())) {
             throw new ClientException("两次密码不一致");
         }
-        stringRedisTemplate.opsForValue().get(RedisCacheConstant.SMS_CODE_CACHE_KEY + requestParam.getPhone());
-        if (!Objects.equals(requestParam.getCode(), stringRedisTemplate.opsForValue().get(RedisCacheConstant.SMS_CODE_CACHE_KEY + requestParam.getPhone()))) {
-            throw new ClientException("验证码错误");
-        }
-
         UserDO userDO = UserDO.builder()
+                .userid(UUID.randomUUID().toString())
                 .username(requestParam.getUsername())
                 .password(requestParam.getPassword1())
                 .phone(requestParam.getPhone())
