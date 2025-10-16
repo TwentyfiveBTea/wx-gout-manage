14c3080f5db6052ae6e4e39f59472281fc278d4d
TwentyfiveBTea <1248102041@qq.com>
2025-10-16 19:38:19 +0800

    修改：更改MyBatisPlus配置的错误

--- src/main/java/com/btea/wxgoutmanage/common/config/MyBatisPlusConfig.java
+++ src/main/java/com/btea/wxgoutmanage/common/config/MyBatisPlusConfig.java
@@ -1,34 +1,28 @@
 package com.btea.wxgoutmanage.common.config;
 
-import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @Author: TwentyFiveBTea
  * @Date: 2025/10/13 09:19
- * @Description: 自动管理事务
+ * @Description: MyBatisPlus配置
  */
 @Configuration
 public class MyBatisPlusConfig {
-    /**
-     * 配置乐观锁插件
-     *
-     * @return 乐观锁插件
-     */
-    @Bean
-    public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor() {
-        return new OptimisticLockerInnerInterceptor();
-    }
 
     /**
-     * 配置分页插件
-     *
-     * @return 分页插件
+     * 分页插件
      */
     @Bean
-    public PaginationInnerInterceptor paginationInnerInterceptor() {
-        return new PaginationInnerInterceptor();
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptorByAdmin() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
